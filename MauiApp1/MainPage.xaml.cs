2b408cb382a611f3e326558f96aaf4b5bd4f6842
DESKTOP-C7MNLCF\Aigi <forusetome@gmail.com>
2024-10-07 06:21:17 +0300

    base

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MauiApp1.Views;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+      
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnStudentsClicked(System.Object sender, System.EventArgs e)
+         => Application.Current.MainPage = new NavigationPage(new Students());
+
+        async void OnEventsClicked(System.Object sender, System.EventArgs e)
+        => Application.Current.MainPage = new NavigationPage(new Events());
+
+    }
+
+}
